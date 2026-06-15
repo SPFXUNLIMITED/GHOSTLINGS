00eef3d8049126ced5d289f24fc6b27eb51d11fe
SPFXUNLIMITED <33075671+SPFXUNLIMITED@users.noreply.github.com>
2026-06-14 23:35:42 -0700

    as
    as

---
+++ error_log.php
@@ -0,0 +1,92 @@
+[10-Jun-2026 15:38:05 America/Los_Angeles] APP_TZ value = 'America/Los_Angeles' | Current PT time = 2026-06-10 15:38:05
+[11-Jun-2026 10:04:49 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/message_image_serve.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[11-Jun-2026 10:31:23 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ''10'' at line 8 in /home3/spfx/public_html/website_a844b2f2/project/messages.php:117
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/messages.php(117): PDOStatement->execute(Array)
+#1 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/messages.php on line 117
+[11-Jun-2026 10:31:26 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ''10'' at line 8 in /home3/spfx/public_html/website_a844b2f2/project/messages.php:117
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/messages.php(117): PDOStatement->execute(Array)
+#1 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/messages.php on line 117
+[11-Jun-2026 10:31:43 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ''10'' at line 8 in /home3/spfx/public_html/website_a844b2f2/project/messages.php:117
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/messages.php(117): PDOStatement->execute(Array)
+#1 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/messages.php on line 117
+[11-Jun-2026 12:11:23 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[11-Jun-2026 15:47:34 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_image.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[12-Jun-2026 10:31:41 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[13-Jun-2026 18:13:27 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/quotes.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[14-Jun-2026 20:42:17 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 689
+[14-Jun-2026 21:21:00 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:14 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:20 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:26 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:29 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:42 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:43 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:44 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:44 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:29:45 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:34:38 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:39:25 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:39:33 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:39:35 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:39:36 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:50:13 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:50:20 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 699
+[14-Jun-2026 21:50:40 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 704
+[14-Jun-2026 21:59:45 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 712
+[14-Jun-2026 22:34:06 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_image.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[14-Jun-2026 23:23:43 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "PO_SHIPPING_TERMS" in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php:222
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php(1394): build_rfq_email_text(Array)
+#1 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php on line 222
+[14-Jun-2026 23:23:51 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "PO_SHIPPING_TERMS" in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php:222
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php(1394): build_rfq_email_text(Array)
+#1 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php on line 222
+[14-Jun-2026 23:24:06 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "PO_SHIPPING_TERMS" in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php:222
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php(1394): build_rfq_email_text(Array)
+#1 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php on line 222
+[14-Jun-2026 23:24:09 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "PO_SHIPPING_TERMS" in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php:222
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php(1394): build_rfq_email_text(Array)
+#1 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php on line 222
