f8409f42ea9ef67a948824fa9cf2746ac0d165b8
SPFXUNLIMITED <33075671+SPFXUNLIMITED@users.noreply.github.com>
2026-06-18 04:17:12 -0700

    Update error_log.php

--- error_log.php
+++ error_log.php
@@ -90,3 +90,115 @@ Stack trace:
 #0 /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php(1394): build_rfq_email_text(Array)
 #1 {main}
   thrown in /home3/spfx/public_html/website_a844b2f2/project/sourcing_rfq_tracker.php on line 222
+[15-Jun-2026 01:18:40 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 712
+[16-Jun-2026 11:24:07 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/quotes.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[17-Jun-2026 11:59:46 America/Los_Angeles] PHP Fatal error:  Uncaught PDOException: SQLSTATE[HY000]: General error: 1359 Trigger already exists in /home3/spfx/public_html/website_a844b2f2/project/db.php:819
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/db.php(819): PDO->exec('\n    CREATE TRI...')
+#1 /home3/spfx/public_html/website_a844b2f2/project/quotes.php(2): require('/home3/spfx/pub...')
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 819
+[17-Jun-2026 12:18:59 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:162
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(438): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(23): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 162
+[17-Jun-2026 12:21:03 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:162
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(438): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(23): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 162
+[17-Jun-2026 12:21:12 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:162
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(438): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(23): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 162
+[17-Jun-2026 14:23:16 America/Chicago] PHP Parse error:  Unclosed '{' on line 412 in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 439
+[17-Jun-2026 12:23:20 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:163
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(439): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 163
+[17-Jun-2026 12:23:25 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:163
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(439): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 163
+[17-Jun-2026 12:24:04 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:163
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(439): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 163
+[17-Jun-2026 12:32:30 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:163
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(439): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 163
+[17-Jun-2026 12:32:58 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:440
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(65): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 440
+[17-Jun-2026 12:33:38 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:440
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(65): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 440
+[17-Jun-2026 12:36:09 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:440
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(65): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 440
+[17-Jun-2026 12:50:52 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:406
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(65): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 406
+[17-Jun-2026 12:51:01 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:406
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(65): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 406
+[17-Jun-2026 12:58:31 America/Los_Angeles] PHP Fatal error:  Uncaught Error: Undefined constant "INVOICE_PAYMENT_STATUS_PAID" in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php:406
+Stack trace:
+#0 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(65): invoice_is_paid(Array)
+#1 /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php(24): invoice_build_email_message_data(Object(PDO), Array, Array, false, NULL)
+#2 {main}
+  thrown in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 406
+[17-Jun-2026 13:11:11 America/Los_Angeles] Invoice email preview failed for quote #20: Undefined constant "INVOICE_PAYMENT_STATUS_PAID"
+[17-Jun-2026 15:19:56 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:15) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:20:08 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:15) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:24:08 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:30:51 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:30:54 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:31:38 America/Chicago] PHP Parse error:  Unclosed '{' on line 708 in /home3/spfx/public_html/website_a844b2f2/project/invoice_form.php on line 718
+[17-Jun-2026 15:31:41 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:34:17 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:34:29 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:36:34 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:36:41 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:37:07 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 15:38:36 America/Chicago] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php:14) in /home3/spfx/public_html/website_a844b2f2/project/db.php on line 19
+[17-Jun-2026 13:43:49 America/Los_Angeles] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/db.php:19) in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php on line 13
+[17-Jun-2026 13:44:26 America/Los_Angeles] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/db.php:19) in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php on line 13
+[17-Jun-2026 13:45:56 America/Los_Angeles] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/db.php:19) in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php on line 13
+[17-Jun-2026 13:48:57 America/Los_Angeles] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/db.php:19) in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php on line 13
+[17-Jun-2026 13:50:14 America/Los_Angeles] PHP Fatal error:  Cannot redeclare app_ensure_integration_settings_table() (previously declared in /home3/spfx/public_html/website_a844b2f2/project/db.php:19) in /home3/spfx/public_html/website_a844b2f2/project/email_preview.php on line 13
+[17-Jun-2026 15:07:55 America/Los_Angeles] PHP Warning:  Undefined variable $can_manage_stage in /home3/spfx/public_html/website_a844b2f2/project/order_form.php on line 712
