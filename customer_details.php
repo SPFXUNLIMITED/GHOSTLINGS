70eb086e840dfccece572506e02d894d0aad791d
SPFXUNLIMITED <33075671+SPFXUNLIMITED@users.noreply.github.com>
2026-09-02 13:41:43 -0700

    Revert customer_details.php to remove broken require of missing module file

--- customer_details.php
+++ customer_details.php
@@ -2,11 +2,7 @@
 require __DIR__ . '/db.php';
 require __DIR__ . '/layout.php';
 require __DIR__ . '/auth.php';
-require_once __DIR__ . '/customer_interaction_module.php';
 require_admin_or_moderator();
-
-customerInteractionEnsureSchema($pdo);
-$customerInteractionCsrf = customerInteractionCsrfToken();
 
 $id = (int)($_GET['id'] ?? 0);
 if ($id <= 0) {
@@ -35,7 +31,6 @@ render_header('Customer Details');
     <div class="actions">
       <a class="btn" href="customers.php">Back to Customers</a>
       <a class="btn" href="customer_form.php?id=<?= (int)$customer['id'] ?>">Edit</a>
-      <button type="button" class="btn" onclick="openCustomerDetailsModal(<?= (int)$customer['id'] ?>)" style="background:#0e7490; color:#fff; border:none;">Interactions</button>
     </div>
   </div>
 </div>
@@ -111,6 +106,4 @@ render_header('Customer Details');
   </table>
 </div>
 
-<?php
-customerInteractionRenderModal();
-render_footer();
+<?php render_footer(); ?>
